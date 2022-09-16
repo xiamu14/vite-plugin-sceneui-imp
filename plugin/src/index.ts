43dbd1ecd9be89c20452fbf3fc65b56e535fd62c
ben <x_z_gfree@163.com>
2022-09-16 15:05:24 +0800

    feature: check style file exists.

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,5 +1,6 @@
 import { Plugin } from "vite";
-
+import * as fs from "fs";
+import * as path from "path";
 export default function vitePluginSceneuiImp(): Plugin {
   return {
     name: "vite-plugin-sceneui-imp",
@@ -7,6 +8,15 @@ export default function vitePluginSceneuiImp(): Plugin {
       if (!/(node_modules)/.test(id) && /@sceneui\/[a-z]{1,}/.test(code)) {
         const matcherArray = code.match(/@sceneui\/[a-z]{1,}/);
         const componentName = (matcherArray && matcherArray[0]) as string;
+        const stylePath = `${componentName}/dist/style.css`;
+        const fullStylePath = path.resolve(
+          process.cwd(),
+          "node_modules",
+          stylePath
+        );
+        const stat = fs.statSync(fullStylePath);
+
+        if (!stat.isFile) return { code, map: null };
         return {
           code: `import '${componentName}/dist/style.css';${code}`,
           map: null,
